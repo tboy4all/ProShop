e1f21838fd2ca1092dc9ef4ca74124ba771586a6
tboy4all <taiyesuleiman09@gmail.com>
2021-09-16 22:55:25 +0100

    Backend User Authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,38 +1,13 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
+//import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+//import Product from '../models/productModel.js'
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js'
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-
-    res.json(products)
-  })
-)
-
-// @desc    Fetch Single products
-// @route   GET /api/products/:id
-// @access  Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    //const product = products.find((p) => p._id === req.params.id)
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.json(product)
-    } else {
-      //   res.status(404).json({ message: 'Product not Found' })
-      res.status(404)
-      throw new Error('Product not found')
-    }
-    //res.json(product)
-  })
-)
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
